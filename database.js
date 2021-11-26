19bd108d3dbd311c2f0c2b1e8ba7bfd31a047315
Matias La Mota <matiaslamota18@gmail.com>
2021-11-26 17:15:32 -0500

    Added correct MongoDB URI

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const chalk = require('chalk');
-const uri = /* 'mongodb+srv://root:LkMU690TSfy@green-complaint.mcj7b.mongodb.net/reports?retryWrites=true&w=majority' */'mongodb://localhost/prueba'; 
+const uri = 'mongodb+srv://root:LkMU690TSfy@green-complaint.mcj7b.mongodb.net/reports?retryWrites=true&w=majority'; 
 
 mongoose.connect(uri, {
     useUnifiedTopology: true,
